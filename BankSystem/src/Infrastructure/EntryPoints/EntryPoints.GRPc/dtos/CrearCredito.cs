4974eea0d31c268e7f171e372ce6a9dd2cb4f54c
Juan Pablo Cano <canojuan0415@gmail.com>
2023-02-24 11:57:55 -0500

    Feat: Credito and Usuario Dto's created

---
+++ BankSystem/src/Infrastructure/EntryPoints/EntryPoints.GRPc/dtos/CrearCredito.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryPoints.GRPc.dtos;
+
+public class CrearCredito
+{
+    public string UsuarioId { get; set; }
+    public string CuentaId { get; set; }
+    public int TotalPrestamo { get; set; }
+    public int DeudaActual { get; set; }
+}
---
+++ BankSystem/src/Infrastructure/EntryPoints/EntryPoints.GRPc/dtos/CrearUsuario.cs
@@ -0,0 +1,11 @@
+﻿namespace EntryPoints.GRPc.dtos;
+
+public class CrearUsuario
+{
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Cedula { get; set; }
+    public string Correo { get; set; }
+    public int Edad { get; set; }
+    public string Profesion { get; set; }
+}
