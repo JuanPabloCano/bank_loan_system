10f79a22426bc98bed07ad57a0afeeebf1ad3191
Juan Pablo Cano <canojuan0415@gmail.com>
2023-02-24 11:57:22 -0500

    Refactor: Fixed service mappings

--- BankSystem/src/Infrastructure/EntryPoints/EntryPoints.GRPc/RPCs/Creditos/RpcServiceCreditos.cs
+++ BankSystem/src/Infrastructure/EntryPoints/EntryPoints.GRPc/RPCs/Creditos/RpcServiceCreditos.cs
@@ -3,8 +3,10 @@ using Domain.Model.Entities.Credito;
 using Domain.Model.Entities.Usuario;
 using Domain.UseCase.Creditos;
 using Domain.UseCase.Usuarios;
+using EntryPoints.GRPc.dtos;
 using EntryPoints.GRPc.RPCs.Base;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 
 namespace EntryPoints.GRPc.RPCs.Creditos;
 
@@ -27,15 +29,17 @@ public class RpcServiceCreditos : GrpcModels.UsuariosServices.UsuariosServicesBa
         ServerCallContext context) => RpcServiceHandler.ProcessResponse(
         async () =>
         {
-            var creditoDto = _mapper.Map<Credito>(request);
-            return await _creditoUseCase.Crear(creditoDto);
+            var creditoDto = _mapper.Map<CrearCredito>(request);
+            var creditoARetornar = await _creditoUseCase.Crear(_mapper.Map<Credito>(creditoDto));
+            return _mapper.Map<GrpcModels.Credito>(creditoARetornar);
         });
 
     public override Task<GrpcModels.StandardResponse> CrearUsuario(GrpcModels.CrearUsuarioRequest request,
         ServerCallContext context) => RpcServiceHandler.ProcessResponse(
         async () =>
         {
-            var usuarioDto = _mapper.Map<Usuario>(request);
-            return await _usuarioUseCase.Crear(usuarioDto);
+            var usuarioDto = _mapper.Map<CrearUsuario>(request);
+            var usuarioARetornar = await _usuarioUseCase.Crear(_mapper.Map<Usuario>(usuarioDto));
+            return _mapper.Map<GrpcModels.Usuario>(usuarioARetornar);
         });
 }
